2971db57c2648a4abe4056aa442c856d890fd336
Sergio Alcala <checo_2k2@hotmail.com>
2016-01-22 10:25:50 -0600

    agregado de ip de prueba

--- app/app.js
+++ app/app.js
@@ -10,8 +10,11 @@ var app = angular.module('app', [
     'uxGenie'
 ]);
 
-// app.constant('api','http://localhost/apimv/public/api/')
-app.constant('api','http://172.17.10.15/apimv/public/api/')
+//ip prueba
+app.constant('api','http://172.17.10.52/apimv/public/api/')
+
+//ip produccion
+// app.constant('api','http://172.17.10.15/apimv/public/api/')
 app.constant('publicfiles','http://172.17.10.15/apimv/public/exports/')
 
 //configuramos las rutas y asignamos html y controlador segun la ruta
