c9b3f7dc61dc7c790919bdd4136136561d6aebdb
medicavial <salcala@medicavial.com.mx>
2016-03-30 18:07:06 -0600

    Cambios en el formulario de Edición de Datos

--- app/controllers/edicionDatosCtrl.js
+++ app/controllers/edicionDatosCtrl.js
@@ -1,4 +1,16 @@
-app.controller('edicionDatosCtrl', function($scope){
+app.controller('edicionDatosCtrl', function($scope) {
 
 	$scope.tituloEdic = "Búsqueda de Folios";
+
+	$scope.Actualiza=function(){
+		$scope.formVisible=false;
+		console.log($scope.formVisible)
+	}
+
+	$scope.formVisible=false;
+
+	$scope.ShowForm=function() {
+		$scope.formVisible=true;
+		console.log($scope.formVisible)
+	}
 });
