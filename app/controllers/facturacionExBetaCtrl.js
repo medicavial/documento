0c24f2a17e9cfbc3ea7971ffc98d62aa4b30f392
Sergio Alcala <checo_2k2@hotmail.com>
2016-11-29 13:33:08 -0600

    se agregaron archivos al proyecto para su mejor entendimiento

---
+++ app/controllers/facturacionExBetaCtrl.js
@@ -0,0 +1,8 @@
+//Area de facturacion
+function facturacionExBetaCtrl($scope, $rootScope, $filter, find , loading, checkFolios,datos,$timeout,facturacionExpress,webStorage,operacion,tickets){
+    
+};
+
+facturacionExBetaCtrl.$inject = ['$scope', '$rootScope', '$filter', 'find', 'loading', 'checkFolios','datos','$timeout','facturacionExpress','webStorage','operacion','tickets'];
+
+app.controller('facturacionExBetaCtrl',facturacionExCtrl);
