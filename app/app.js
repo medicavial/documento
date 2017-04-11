feb06c842d8a6784db9c249045a934967118cd61
medicavial <salcala@medicavial.com.mx>
2017-04-11 14:01:19 -0500

    listado pagos
    nuevo campo en listado pagos

--- app/app.js
+++ app/app.js
@@ -16,10 +16,10 @@ var app = angular.module('app', [
 ]);
 
 //ip prueba
-app.constant('api','http://172.17.10.58/apimv/public/api/')
-app.constant('publicfiles','http://172.17.10.58/apimv/public/exports/')
-app.constant('xml','http://172.17.10.58/apimv/public/FacturasPagos/')
-app.constant('xmlFE','http://172.17.10.58/apimv/public/FacturasPagos/xmltemporal/')
+// app.constant('api','http://172.17.10.58/apimv/public/api/')
+// app.constant('publicfiles','http://172.17.10.58/apimv/public/exports/')
+// app.constant('xml','http://172.17.10.58/apimv/public/FacturasPagos/')
+// app.constant('xmlFE','http://172.17.10.58/apimv/public/FacturasPagos/xmltemporal/')
 
 // app.constant('api','http://localhost/apimv/public/api/')
 // app.constant('publicfiles','http://localhost/apimv/public/exports/')
@@ -27,10 +27,10 @@ app.constant('xmlFE','http://172.17.10.58/apimv/public/FacturasPagos/xmltemporal
 // app.constant('xmlFE','http://localhost/apimv/public/FacturasPagos/xmltemporal/')
 
 //ip produccion
-// app.constant('api','http://172.17.10.15/apimv/public/api/')
-// app.constant('publicfiles','http://172.17.10.15/apimv/public/exports/');
-// app.constant('xml','http://172.17.10.15/apimv/public/FacturasPagos/')
-// app.constant('xmlFE','http://172.17.10.15/apimv/public/FacturasPagos/xmltemporal/')
+app.constant('api','http://172.17.10.15/apimv/public/api/')
+app.constant('publicfiles','http://172.17.10.15/apimv/public/exports/');
+app.constant('xml','http://172.17.10.15/apimv/public/FacturasPagos/')
+app.constant('xmlFE','http://172.17.10.15/apimv/public/FacturasPagos/xmltemporal/')
 
 // var xml = new Firebase("http://www.medicavial.net/registro");
 
