a2aeec7759c7e111ffd90919ea83da1d98f01b3b
medicavial <salcala@medicavial.com.mx>
2017-04-28 16:19:42 -0500

    subir acuses
    fin del módulo para subir acuses de unidades

--- app/controllers/subirDocumentosCtrl.js
+++ app/controllers/subirDocumentosCtrl.js
@@ -3,16 +3,17 @@ function subirDocumentosCtrl($scope, $rootScope,  loading, $filter, $timeout, fi
   $scope.unidades ='';
   $scope.digitales ={
       unidad:'',
-      fecha:'',
+      fecha:fecha(),
       tipo:"1"
   }
+  $scope.mensaje='';
 
   $scope.file='';
 
     $scope.inicio = function()
     {
         $scope.tituloPT = "Subir Documentos";
-        find.unidades().success(function (data){
+        find.unidadesweb().success(function (data){
           console.log(data);
           $scope.unidades =data;
         });
@@ -21,18 +22,42 @@ function subirDocumentosCtrl($scope, $rootScope,  loading, $filter, $timeout, fi
     //función para subir archivos
     $scope.uploadFile = function()
   	{
-      console.log($scope.digitales.unidad);
+      loading.cargando('Subiendo Documento');
       var unidad = $scope.digitales.unidad;
       var fecha = $scope.digitales.fecha;
   		var file = $scope.file;
       var fechaMod = fecha.replace(/\//g,"-");
-      console.log(fechaMod);
-      console.log(unidad+'----'+file+'----'+fechaMod);
   		upload.uploadFile(file, unidad, fechaMod).then(function(res)
   		{
-  			console.log(res);
+        console.log(res);
+        if(res.data=='ok'){
+          $scope.digitales ={
+              unidad:'',
+              fecha:'',
+              tipo:"1"
+          }
+          $scope.file='';
+          $scope.mensaje='El documento se subió correctamente';
+        }else{
+          $scope.mensaje='Ocurrió un problema';
+        }
+        loading.despedida();
   		})
   	}
+}
+function fecha(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd;
+    }
+    if(mm<10){
+        mm='0'+mm;
+    }
+    var today = dd+'/'+mm+'/'+yyyy;
+    return today;
 }
 subirDocumentosCtrl.$inject =['$scope', '$rootScope', 'loading','$filter','$timeout','find','upload'];
 app.controller('subirDocumentosCtrl',subirDocumentosCtrl);
